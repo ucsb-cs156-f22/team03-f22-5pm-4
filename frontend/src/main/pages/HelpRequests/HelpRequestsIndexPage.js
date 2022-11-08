e29cac9f90feb19e254c66ca660f3758f2ed64b5
Cyril Wang <cyrilwang@umail.ucsb.edu>
2022-11-07 16:27:45 -0800

    added Help Requests Index, Edit, and Create placeholder pages

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.js
@@ -1,10 +1,10 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function TodosIndexPage() {
+export default function HelpRequestsIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>help req index</h1>
+        <h1>Help Requests</h1>
         <p>
           This is where the index page will go
         </p>
