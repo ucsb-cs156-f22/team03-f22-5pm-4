715d25b0bbc5b185b670a4a4f1ca1f05c8310759
Omkar Lonkar <omkarlonkar@umail.ucsb.edu>
2022-11-14 13:26:02 -0800

    having issues with storybook and linking table with index page

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "after hours - the weeknd (sped up)",
+        "url": "https://www.youtube.com/watch?v=WccqQ_sWfVY",
+        "explanation": "weeknd",
+        "email": "abel@gmail.com",
+        "dateAdded": "2022-01-02T12:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 2,
+            "title": "hot n cold [sped up]",
+            "url": "https://www.youtube.com/watch?v=F_U2nxULfQA",
+            "explanation": "katy",
+            "email": "kperry@gmail.com",
+            "localDateTime": "2022-01-02T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "Just dance (sped up)",
+            "url": "https://www.youtube.com/watch?v=V0UZd0cccDw",
+            "explanation": "Gaga",
+            "email": "ladyg@gmail.com",
+            "localDateTime": "2022-04-03T12:00:00"
+        },
+        {
+            "id": 4,
+            "title": "give it up to me - sean paul (sped up)",
+            "url": "https://www.youtube.com/watch?v=rugmvJKkF30",
+            "explanation": "sean",
+            "email": "spaul@gmail.com",
+            "localDateTime": "2022-07-04T12:00:00"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
