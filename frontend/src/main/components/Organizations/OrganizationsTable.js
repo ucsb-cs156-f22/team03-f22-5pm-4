2556ada89dd246576b33e7b51454327e31274d1a
Geivet <lgeivet@ucsb.edu>
2022-11-14 14:51:37 -0800

    lg - added Utils for delete function

--- frontend/src/main/components/Organizations/OrganizationsTable.js
+++ frontend/src/main/components/Organizations/OrganizationsTable.js
@@ -1,6 +1,6 @@
 import OurTable, {ButtonColumn} from "main/components/OurTable";
 import { useBackendMutation } from "main/utils/useBackend";
-import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import {  onDeleteSuccess } from "main/utils/OrganizationsUtils"
 import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
