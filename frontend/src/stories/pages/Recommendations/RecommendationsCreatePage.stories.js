141e95b4a6ca61105b22cb226c7788fda6134eb4
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2022-11-14 16:38:49 -0800

    ar - added pages stores folder

---
+++ frontend/src/stories/pages/Recommendations/RecommendationsCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import RecommendationsCreatePage from "main/pages/Recommendations/RecommendationsCreatePage";
+
+export default {
+    title: 'pages/Recommendations/RecommendationsCreatePage',
+    component: RecommendationsCreatePage
+};
+
+const Template = () => <RecommendationsCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
---
+++ frontend/src/stories/pages/Recommendations/RecommendationsEditPage.stories.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import RecommendationsEditPage from "main/pages/Recommendations/RecommendationsEditPage";
+
+export default {
+    title: 'pages/Recommendations/RecommendationsEditPage',
+    component: RecommendationsEditPage
+};
+
+const Template = () => <RecommendationsEditPage />;
+
+export const Default = Template.bind({});
+
+
+
