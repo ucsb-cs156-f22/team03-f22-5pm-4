4d2b22228c5a3e7e86f76703cf16177317a1ea74
BaronLuisYoung <76130571+BaronLuisYoung@users.noreply.github.com>
2022-11-15 16:30:57 -0800

    something2

--- frontend/src/stories/components/MenuItems/MenuItemsTable.stories.js
+++ frontend/src/stories/components/MenuItems/MenuItemsTable.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import MenuItemTable from "main/components/MenuItems/MenuItemTable";
 import { menuItemFixtures } from 'fixtures/menuItemFixtures';
-import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+// import { currentUserFixtures } from 'fixtures/currentUserFixtures';
 
 export default {
     title: 'components/MenuItems/MenuItemsTable',
